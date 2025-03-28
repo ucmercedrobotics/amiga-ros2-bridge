02d4b6577411d4b177e34a46f35e3fe5dd5642d8
MScatolin <marceloscatolin.queiroz@gmail.com>
2025-03-28 11:27:45 -0700

    removed robot addresses and formatting

--- amiga_ros2_bridge/amiga_ros2_bridge/clock.py
+++ amiga_ros2_bridge/amiga_ros2_bridge/clock.py
@@ -5,15 +5,17 @@ from rclpy.node import Node
 from geometry_msgs.msg import TwistStamped
 from rosgraph_msgs.msg import Clock
 
+
 class PublishClock(Node):
     def __init__(self):
-        super().__init__('clock')
+        super().__init__("clock")
 
         self.sub_canbus_twist = self.create_subscription(
-            TwistStamped, '/canbus/twist', self.canbus_twist_callback, 10)
-      
-        self.clock_pub = self.create_publisher(Clock, '/clock', 10)
-    
+            TwistStamped, "/canbus/twist", self.canbus_twist_callback, 10
+        )
+
+        self.clock_pub = self.create_publisher(Clock, "/clock", 10)
+
     def canbus_twist_callback(self, msg):
         self.publish_clock(msg.header.stamp)
 
@@ -23,6 +25,7 @@ class PublishClock(Node):
         self.clock_pub.publish(clock_msg)
         self.get_logger().info(f"Published timestamp to /clock: {stamp}")
 
+
 def main(args=None):
     rclpy.init(args=args)
     node = PublishClock()
@@ -30,5 +33,6 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
