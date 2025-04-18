a1f016c42ddd777e73b5b2a1aa4af8dd124c40cc
Anas Saeed <saeedanas396@gmail.com>
2025-04-17 20:18:04 -0700

    changed package name in launch file

--- amiga_ros2_teleop/launch/twist_joy.launch.py
+++ amiga_ros2_teleop/launch/twist_joy.launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             Node(
-                package="amiga_ros2_bridge",
+                package="amiga_ros2_teleop",
                 executable="twist_joy",
                 name="twist_joy",
             )
